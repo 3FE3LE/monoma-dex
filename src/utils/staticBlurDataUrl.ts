eda23e3b81c13eadaab3985f218de2e2faafba7e
Frank Licona <3l33f3@gmail.com>
2023-09-15 20:53:08 -0500

    rename index utils file & and staticBlurData to optimize images

---
+++ src/utils/staticBlurDataUrl.ts
@@ -0,0 +1,17 @@
+export function staticBlurDataRyl() {
+  const blurSvg = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 5">
+    <filter id='b' color-interpolation-filters='sRGB'>
+    <feGaussianBlur stdDeviation='1'/>
+    </filter>
+    <rect preserveAspectRatio='none' stroke-width="3" stroke="#7b9fa6" x='0' y='0' width='100%' height='100%' filter='url(#b)' fill='#426874'/>
+  </svg>
+  `
+
+  const toBase64 = (str: string) =>
+    typeof window === 'undefined'
+      ? Buffer.from(str).toString('base64')
+      : window.btoa(str)
+
+  return `data:image/svg+xml;base64,${toBase64(blurSvg)}`
+}

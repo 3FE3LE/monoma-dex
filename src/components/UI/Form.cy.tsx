d6f0385b82cd8679397789808fe7c7be328db5fc
Frank Licona <3l33f3@gmail.com>
2023-09-16 12:08:51 -0500

    fix form.cy.tsx missings component props

--- src/components/UI/Form.cy.tsx
+++ src/components/UI/Form.cy.tsx
@@ -3,7 +3,10 @@ import Form from './Form'
 
 describe('<Form />', () => {
   it('renders', () => {
+    const formTitle = 'Register'
+    const onClick = cy.stub()
+    const children = <></>
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Form />)
+    cy.mount(<Form title={formTitle} handleSubmit={onClick} >{children}</Form>)
   })
 })
